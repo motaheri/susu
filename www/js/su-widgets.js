1d6413ec77e93e53dda555959468d04b9916d3d5
Michael Nicholas <me@michaelnicholas.co.uk>
2013-03-09 20:38:33 +0000

    Work in progress
    Shouldn't really be a commit, but I want to check it in.

---
+++ www/js/su-widgets.js
@@ -0,0 +1,29 @@
+var SU_Widget = {
+	BlogWidget: function(mslWidgetId, targetSelector, number) {
+		if (typeof SU_Data == "undefined") {
+			console.error("SU_Widget: SU_Data not defined.");
+			return;
+		}
+		if (typeof SU_Data.blogData[mslWidgetId] == "undefined") {
+			console.error("SU_Widget: SU_Data doesn't contain a blog called '" + mslWidgetId + "'");
+			return;
+		}
+		if ($(targetSelector).length != 1) {
+			console.error("SU_Widget: Blog target object invalid '" + targetSelector + "'");
+			return;
+		}
+		jQuery.each(SU_Data.blogData[mslWidgetId], function(i, o) {
+			if (i >= number) return;
+			var post = $(document.createElement('div')).addClass('su-blog-post');
+			var inner = $(document.createElement('div')).addClass('su-blog-post-inner');
+			var title = $(document.createElement('div')).addClass('title').text(o.Title);
+			var intro = $(document.createElement('div')).addClass('intro').text(o.Description);
+			var image = $(document.createElement('img')).addClass('author').attr('src', 'http://placehold.it/200x200');
+			$(inner).append(title);
+			$(inner).append(intro);
+			$(inner).append(image);
+			$(post).append(inner);
+			$(targetSelector).append(post);
+		});
+	}
+};
