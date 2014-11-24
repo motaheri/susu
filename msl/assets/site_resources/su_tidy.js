ef246cce16f7c22f33bc81f28bb9e6d8bc09b7a8
patrickhartesu <patrick.harte@gmail.com>
2014-11-24 17:11:18 +0000

    Tweak to su_tidy.js, various images for Travel Shop event, eventlist

--- msl/assets/site_resources/su_tidy.js
+++ msl/assets/site_resources/su_tidy.js
@@ -1,4 +1,3 @@
-<script type="text/javascript">
 /*
 	This little file tidies up the hideous overflowing Facebook links and unused social media boxes on the right of some pages.
 	I could have gone rooting through a ton of pre-existing jQuery looking for where the contents of these boxes is set, 
@@ -7,15 +6,62 @@
 */
 $(document).ready(function()
 {
-	console.log("yup");
 	$('.varsityLinkIcon a.msl_web').each(function(index) 
 	{
-		console.log("yupyup");
-		var pattern = new RegExp('.*facebook.*');
+		var fbPattern = 	new RegExp(/((https?:\/\/)|(www\.)|(https?:\/\/www\.))?facebook\.[a-z\.]{2,}\/?(([a-z\d]+\/?)*(\.[a-z]{2,})?)(\?\w+\=\w+)*/);
+		//var fbPattern = 	new RegExp(/.*facebook.*/); //IDIOT MODE ENGAGED
+		var otherPattern = 	new RegExp(/((https?:\/\/)|(www\.)|(https?:\/\/www\.))([a-z\d\.\-]+)+\.[a-z\.]{2,}\/?(([a-z\d]+\/?)*(\.[a-z]{2,})?)(\?\w+\=\w+)*/);
 		var hrefAttribute = $(this).attr('href');
+		if (fbPattern.test(hrefAttribute))
+		{
+			console.log('yup');
+			$(this).html('Visit our Facebook page');
+		}
+		else
+		{
+			if (otherPattern.test(hrefAttribute))
+			{
+			console.log('yupyup');
+				$(this).html('Visit our website');
+			}
+			else
+			{
+				/*
+					This is the nuclear option, but if someone messes 
+					up their URL really badly I'd rather have the box 
+					disappear than leave it lying around all malformed. For starters 
+					they're more likely to email someone and ask WTF happened.
+					
+					This code as it stands should grab the varsityLinkIcon div 
+					and hide it.
+				*/
+				console.log('yupyupyup');
+				$(this).parent.css('display', 'none');
+			}
+		}
+	});
+
+	$('.varsityEmailIcon a.msl_email').each(function(index) 
+	{
+		console.log('email');
+		var pattern = new RegExp(/mailto.*@swansea-union\.co\.uk/);
+		console.log('pattern: ' + pattern);
+		var hrefAttribute = $(this).attr('href');
+		console.log('hrefAttribute: ' + hrefAttribute);
 		if (pattern.test(hrefAttribute))
 		{
-			$(this).html('Visit our Facebook page');
+		console.log('emailemail');
+			$(this).html('Email the committee');
+		}
+		else
+		{
+			/*
+				use our email system or get lost!
+				...this probably needs to go, but it's 
+				late so I'm not taking out the conditional
+			*/
+		console.log('emailemailemail');
+			$(this).parent.css('display', 'none');
 		}
 	});
 
@@ -26,6 +72,6 @@ $(document).ready(function()
 		{
 			$(this).css('display', 'none');
 		}
+		
 	});
 });
-</script>
